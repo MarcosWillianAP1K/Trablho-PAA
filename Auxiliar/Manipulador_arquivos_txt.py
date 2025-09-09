75751e4639f2614967d03e18d855e5cc2fcecbc6
MarcosWillianAP1K <mwss@outlook.com>
2025-09-08 23:10:40 -0300

    Escrita dos resultados no txt prontos

--- Auxiliar/Manipulador_arquivos_txt.py
+++ Auxiliar/Manipulador_arquivos_txt.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 
 
@@ -75,18 +76,56 @@ def ler_numeros_do_arquivo(nome_arquivo:str):
 
 def escrever_no_arquivo(nome_arquivo:str, conteudo:str):
     """
-    Escreve o conteúdo fornecido em um arquivo de texto.
+    Verifica se o arquivo existe. Se existir, escreve no arquivo existente;
+    caso contrário, cria um novo arquivo.
 
     Args:
         nome_arquivo (str): O nome do arquivo onde o conteúdo será escrito.
         conteudo (str): O conteúdo a ser escrito no arquivo.
     """
     try:
-        with open(nome_arquivo, 'w') as arquivo:
+        mode = 'a' if os.path.exists(nome_arquivo) else 'w'
+        with open(nome_arquivo, mode) as arquivo:
             arquivo.write(conteudo)
-        print(f"Conteúdo escrito com sucesso no arquivo '{nome_arquivo}'.")
+        if mode == 'a':
+            print(f"Conteúdo adicionado ao arquivo existente '{nome_arquivo}'.")
+        else:
+            print(f"Novo arquivo '{nome_arquivo}' criado e conteúdo escrito com sucesso.")
     except Exception as e:
         print(f"Erro ao escrever no arquivo '{nome_arquivo}': {e}")
+
+
+def apagar_arquivo(nome_arquivo:str):
+    """
+    Apaga o arquivo especificado, se ele existir.
+
+    Args:
+        nome_arquivo (str): O nome do arquivo a ser apagado.
+    """
+    try:
+        if os.path.exists(nome_arquivo):
+            os.remove(nome_arquivo)
+            print(f"Arquivo '{nome_arquivo}' apagado com sucesso.")
+        else:
+            print(f"O arquivo '{nome_arquivo}' não existe.")
+    except Exception as e:
+        print(f"Erro ao apagar o arquivo '{nome_arquivo}': {e}")
+
+def limpar_arquivo(nome_arquivo:str):
+    """
+    Limpa o conteúdo do arquivo especificado, se ele existir.
+    Se o arquivo não existir, cria um novo arquivo vazio.
+
+    Args:
+        nome_arquivo (str): O nome do arquivo a ser limpo ou criado.
+    """
+    try:
+        if os.path.exists(nome_arquivo):
+            with open(nome_arquivo, 'w') as arquivo:
+                arquivo.truncate(0)
+                
+    except Exception as e:
+        pass
 
 
 # if __name__ == "__main__":
--- Main.py
+++ Main.py
@@ -1,100 +1,96 @@
 from Auxiliar import Cronometro 
-from Auxiliar import Gerador_de_testes
+from Auxiliar import Manipulador_arquivos_txt
 from Algoritmos import Coktail
 from Algoritmos import Radix
 
-
+#Defina na lista cada indice indicando a quantidade de numero do arquivo de teste, e a quantidade de indices indica quantos testes serao feitos
+lista_quantidade_de_testes = ["100"]
 
 def executar_testes_coktail():
-    
-    lista_crescente = Gerador_de_testes.ler_numeros_do_arquivo('Arquivos_testes/crescentes_100.txt')
-    lista_decrescente = Gerador_de_testes.ler_numeros_do_arquivo('Arquivos_testes/decrescentes_100.txt')
-    lista_aleatoria = Gerador_de_testes.ler_numeros_do_arquivo('Arquivos_testes/aleatorios_100.txt')
-    
-    cronometro = Cronometro.Cronometro()
-    
-    cronometro.iniciar()
-    
-    Coktail.cocktail_sort(lista_crescente)
-    
-    cronometro.parar()
 
-    print(f"\nTempo de execução do Cocktail Sort para lista crescente: {cronometro.tempo_milisegundos()} milissegundos")
-    
-    cronometro.resetar()
-    
-    
-    
-    cronometro.iniciar()
-    
-    Coktail.cocktail_sort(lista_decrescente)
-    
-    cronometro.parar()
-    
-    print(f"Tempo de execução do Cocktail Sort para lista decrescente: {cronometro.tempo_milisegundos()} milissegundos")
-    
-    cronometro.resetar()
-    
-    
-    cronometro.iniciar()
-    
-    Coktail.cocktail_sort(lista_aleatoria)
-    
-    cronometro.parar()
-    
-    print(f"Tempo de execução do Cocktail Sort para lista aleatória: {cronometro.tempo_milisegundos()} milissegundos")
+    #Para cada quantidade ler o arquivo correspondente (Ja padronizado na funcao de gerar arquivos so mudando a quantidade)
+    for i in lista_quantidade_de_testes:
+        
+        Manipulador_arquivos_txt.limpar_arquivo(f'Resultados/Resultados_coktail_{i}.txt')
+        
+        lista_crescente = Manipulador_arquivos_txt.ler_numeros_do_arquivo(f'Arquivos_testes/crescentes_{i}.txt')
+        lista_decrescente = Manipulador_arquivos_txt.ler_numeros_do_arquivo(f'Arquivos_testes/decrescentes_{i}.txt')
+        lista_aleatoria = Manipulador_arquivos_txt.ler_numeros_do_arquivo(f'Arquivos_testes/aleatorios_{i}.txt')
+        
+        print()
+        
+        def testar_lista(tipo_lista:str, lista:list[int]):
+            cronometro = Cronometro.Cronometro()
+            cronometro.iniciar()
+            Coktail.cocktail_sort(lista)
+            cronometro.parar()
+            tempo_milisegundos = cronometro.tempo_milisegundos()
+            print(f"Tempo de execução do Cocktail Sort para lista {tipo_lista} com {i} elementos: {tempo_milisegundos} milissegundos")
+            cronometro.resetar()
+            
+            return tempo_milisegundos
+        
+        resultado_crescente = testar_lista("crescente", lista_crescente)
+        resultado_decrescente = testar_lista("decrescente", lista_decrescente)
+        resultado_aleatoria = testar_lista("aleatória", lista_aleatoria)
+        
+        print()
 
-    cronometro.resetar()
-
+        Manipulador_arquivos_txt.escrever_no_arquivo(f'Resultados/Resultados_coktail_{i}.txt',
+                                                     f"Resultados do Cocktail Sort para listas com {i} elementos:\n\n"
+                                                     f"Lista crescente: {resultado_crescente} milissegundos\n"
+                                                     f"Lista decrescente: {resultado_decrescente} milissegundos\n"
+                                                     f"Lista aleatoria: {resultado_aleatoria} milissegundos\n\n"
+                                                     f"---------------------------------\n\n")
+        
+        print()
 
 
 def executar_testes_radix():
     
-    lista_crescente = Gerador_de_testes.ler_numeros_do_arquivo('Arquivos_testes/crescentes_100.txt')
-    lista_decrescente = Gerador_de_testes.ler_numeros_do_arquivo('Arquivos_testes/decrescentes_100.txt')
-    lista_aleatoria = Gerador_de_testes.ler_numeros_do_arquivo('Arquivos_testes/aleatorios_100.txt')
-    
-    
-    
-    cronometro = Cronometro.Cronometro()
-    
-    cronometro.iniciar()
-    
-    Radix.radix_sort(lista_crescente)
-    
-    cronometro.parar()
+    #Para cada quantidade ler o arquivo correspondente (Ja padronizado na funcao de gerar arquivos so mudando a quantidade)
+    for i in lista_quantidade_de_testes:
+        
+        Manipulador_arquivos_txt.limpar_arquivo(f'Resultados/Resultados_radix_{i}.txt')
+        
+        lista_crescente = Manipulador_arquivos_txt.ler_numeros_do_arquivo(f'Arquivos_testes/crescentes_{i}.txt')
+        lista_decrescente = Manipulador_arquivos_txt.ler_numeros_do_arquivo(f'Arquivos_testes/decrescentes_{i}.txt')
+        lista_aleatoria = Manipulador_arquivos_txt.ler_numeros_do_arquivo(f'Arquivos_testes/aleatorios_{i}.txt')
+        
+        print()
+        
+        def testar_lista(tipo_lista:str, lista:list[int]):
+            cronometro = Cronometro.Cronometro()
+            cronometro.iniciar()
+            Radix.radix_sort(lista)
+            cronometro.parar()
+            tempo_milisegundos = cronometro.tempo_milisegundos()
+            print(f"Tempo de execução do Radix Sort para lista {tipo_lista} com {i} elementos: {tempo_milisegundos} milissegundos")
+            cronometro.resetar()
+            
+            return tempo_milisegundos
 
-    print(f"\nTempo de execução do Radix Sort para lista crescente: {cronometro.tempo_milisegundos()} milissegundos")
-    
-    cronometro.resetar()
-    
-    
-    
-    cronometro.iniciar()
-    
-    Radix.radix_sort(lista_decrescente)
-    
-    cronometro.parar()
-    
-    print(f"Tempo de execução do Radix Sort para lista decrescente: {cronometro.tempo_milisegundos()} milissegundos")
-    
-    cronometro.resetar()
-    
-    
-    cronometro.iniciar()
-    
-    Radix.radix_sort(lista_aleatoria)
-    
-    cronometro.parar()
-    
-    print(f"Tempo de execução do Radix Sort para lista aleatória: {cronometro.tempo_milisegundos()} milissegundos")
+        resultado_crescente = testar_lista("crescente", lista_crescente)
+        resultado_decrescente = testar_lista("decrescente", lista_decrescente)
+        resultado_aleatoria = testar_lista("aleatória", lista_aleatoria)
+        
+        print()
+        
+        Manipulador_arquivos_txt.escrever_no_arquivo(f'Resultados/Resultados_radix_{i}.txt',
+                                                     f"Resultados do Radix Sort para listas com {i} elementos:\n\n"
+                                                     f"Lista crescente: {resultado_crescente} milissegundos\n"
+                                                     f"Lista decrescente: {resultado_decrescente} milissegundos\n"
+                                                     f"Lista aleatoria: {resultado_aleatoria} milissegundos\n\n"
+                                                     f"---------------------------------\n\n")
+        
+        print()
 
-    cronometro.resetar()
-    
-    
 
 
 if __name__ == '__main__':
+    
+    
+    
     executar_testes_coktail()
     print("\n---------------------------------\n")
     executar_testes_radix()
