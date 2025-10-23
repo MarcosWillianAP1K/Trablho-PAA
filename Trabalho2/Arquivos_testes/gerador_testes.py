ce57a4b09d672cbd079e753b0b933d1a6788d612
MarcosWillianAP1K <mwss@outlook.com>
2025-10-22 23:17:07 -0300

    Criando a configuração do path.

---
+++ Trabalho2/Arquivos_testes/gerador_testes.py
@@ -0,0 +1,7 @@
+import sys
+from pathlib import Path
+
+# Adiciona temporariamente o pai ao path para importar setup_path
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
+import setup_path #configura o path automaticamente
---
+++ Trabalho2/setup_path.py
@@ -0,0 +1,27 @@
+import sys
+from pathlib import Path
+
+def encontrar_raiz_projeto(nome_pasta:str) -> Path:
+    """Encontra a pasta raiz do projeto subindo na hierarquia"""
+    caminho_atual = Path(__file__).resolve()
+    
+    for pasta in caminho_atual.parents:
+        if pasta.name == nome_pasta:
+            return pasta
+    
+    return Path(__file__).parent
+
+def configurar_path(nome_pasta:str= ""):
+    
+    if nome_pasta == "":
+        return
+    
+    """Configura o sys.path para permitir imports do projeto"""
+    diretorio_projeto = encontrar_raiz_projeto(nome_pasta)
+    if str(diretorio_projeto) not in sys.path:
+        sys.path.insert(0, str(diretorio_projeto))
+    
+
+#configurar automaticamente ao importar
+configurar_path("Trabalho2")
+
